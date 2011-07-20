9c504815fe5cc35517ffe39a28c20008d9316db5
Danielfeng <danielfancy@gmail.com>
2011-07-20 13:06:50 +0800

    grep color

--- sbin/init_bashrc.sh
+++ sbin/init_bashrc.sh
@@ -3,8 +3,8 @@
 # E-mail : danielfancy@gmail.com
 
 echo 'PS1="\[\e[01;33m\A \e[01;35m\u\e[01;30m@\e[01;32m\h\] \e[0m[\e[01;34m\W\e[0m] "' >> /root/.bashrc
+echo "alias grep='grep --color=auto'" >> /root/.bashrc
 
-echo "export alias grep='grep --color=auto'" >> /root/.bashrc
 echo "export COREMAIL_HOME=/home/coremail" >> /root/.bashrc
 echo "export JAVA_HOME=/home/coremail/java/jre1.6.0_11" >> /root/.bashrc
 echo "export TOMCAT_HOME=/home/coremail/java/tomcat" >> /root/.bashrc
