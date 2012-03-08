74419ed0f64180a45e2430a2de4e28e7d3bf4dfe
Danielfeng <danielfancy@gmail.com>
2012-03-08 18:05:49 +0800

    add repo

--- sbin/init_EPEL.sh
+++ sbin/init_EPEL.sh
@@ -2,4 +2,9 @@
 # Author : danielfeng
 # E-Mail : danielfancy@gmail.com
 
+CONF=/home/ctools/conf
+YUM=/etc/yum.repos.d/
+
 rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm
+
+cp $CONF/CentOS-Base.repo $YUM
