1e2278fb9649b4a263aada658a692c1abebe6d0b
Daniel <danielfancy@gmail.com>
2012-03-11 19:14:43 +0800

    update shutdown service

--- sbin/init_rc.sh
+++ sbin/init_rc.sh
@@ -7,3 +7,42 @@ service iptables stop
 
 chkconfig sendmail off
 chkconfig iptables off
+
+
+chkconfig portmap off     	#NIS 和NFS 提供动态端口的分配
+chkconfig cups off       	#Common UNIX Printing System
+chkconfig nfslock off		#NFS file lock
+chkconfig apmd off		#Advanced Power Management
+chkconfig bluetooth off		
+chkconfig hidd off		#Bluetooth H.I.D service
+chkconfig gpm off		#General Purpose Mouse Daemon
+chkconfig ip6tables off		
+chkconfig pcscd off		#读卡器	
+chkconfig xfs off		#x font server
+#chkconfig netfs off			
+chkconfig autofs off		#cd auto
+chkconfig avahi-daemon off      #发现基于zeroconf协议的设备
+chkconfig auditd off		#审核守护进程
+#chkconfig haldaemon off
+chkconfig rpcidmapd off		#redhat update software
+chkconfig rpcgssd   off		#redhat update software
+chkconfig isdn off		#isdn modem
+
+service portmap stop
+service cups  
+service nfslock stop
+service apmd stop
+service bluetooth stop
+service hidd stop
+service gpm stop
+service ip6tables stop
+service pcscd stop
+service xfs stop
+#service netfs stop
+service autofs stop
+service avahi-daemon stop
+service auditd stop
+#service haldaemon stop
+service rpcidmapd stop
+service rpcgssd stop
+service isdn stop
