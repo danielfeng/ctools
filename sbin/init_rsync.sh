e18524cda9c8d7134480562fcd5c21cc56e8f0e0
Daniel <danielfancy@gmail.com>
2012-03-13 22:14:21 +0800

    add rsync conf

---
+++ sbin/init_rsync.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Author : danielfeng
+# E-Mail : danielfancy@gmail.com
+
+
+CONF=/home/ctools/conf
+RSYNC=/etc/
+
+
+
+if [ -f ${YUM}/rsyncd.conf ] ; then
+    mv ${YUM}/rsyncd.conf ${YUM}/rsyncd.conf.original
+    cp ${CONF}/rsyncd.conf ${YUM}
+    chmod 644 ${YUM}/rsyncd.conf
+    chown root:root ${YUM}/rsyncd.conf
+else
+    cp ${CONF}/rsyncd.conf ${YUM}
+    chmod 644 ${YUM}/rsyncd.conf
+    chown root:root ${YUM}/rsyncd.conf
+fi
