ef2a1598bacfb5716c3239783c4bc16d6f59e422
Daniel <danielfancy@gmail.com>
2012-03-14 21:55:14 +0800

    update dssh

--- bin/dssh.sh
+++ bin/dssh.sh
@@ -3,8 +3,9 @@
 # E-Mail : danielfancy@gmail.com
 
 HOSTS=$(grep IP= /home/coremail/conf/hosts.cf | awk -F\" '{print $2}')
+#TT=`grep "\[" /home/hosts.cf`
 
-select ip in $HOSTS; do
+select ip in $HOSTS ; do
     if [ -n "$ip" ] ; then
         ssh $ip 
     fi
