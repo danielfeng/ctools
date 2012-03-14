484fca44d95e1c32188c76ffb7b048a4b9a693b1
Daniel <danielfancy@gmail.com>
2012-03-14 21:08:24 +0800

    update backup coremail

--- cmbin/backup_coremail.sh
+++ cmbin/backup_coremail.sh
@@ -6,7 +6,12 @@ COREMAIL=/home/coremail
 DATE=`date +%y%m%d%H`
 
 BACKUP_LIST="api bin conf java lib libexec web"
+BAK_DIR=/home/cmbackup
+
+if [ ! -d ${BAK_DIR} ] ; then
+    mkdir -p ${BAK_DIR}
+fi
 
 cd $COREMAIL
-tar czvf /home/coremailbk$DATE.tgz $BACKUP_LIST
+tar czvf ${BAK_DIR}/coremailbk$DATE.tgz $BACKUP_LIST
 
