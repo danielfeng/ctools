998fe8dbd1fb626ce11e3bc7f93ea6472e426d2f
Danielfeng <danielfancy@gmail.com>
2012-10-31 13:40:51 +0800

    update init_yum

--- sbin/init_yum.sh
+++ sbin/init_yum.sh
@@ -21,11 +21,11 @@ fi
 
 #rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm
 
-if [ $OS -eq 64 ]; then
-    rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-7.noarch.rpm
-else
-    rpm -ivh http://dl.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm
-fi
+#if [ $OS -eq 64 ]; then
+#    rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-7.noarch.rpm
+#else
+#    rpm -ivh http://dl.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm
+#fi
 
 
 
