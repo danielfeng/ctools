b98712896d5937ce11c8c905b30c781d43a7a3fd
Danielfeng <danielfancy@gmail.com>
2012-11-04 19:21:58 +0800

    add coremail's hostname and ip to hosts

---
+++ cmbin/add_cmhosts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Author : danielfeng
+# E-Mail : danielfancy@gmail.com
+
+CMHOSTS=/home/home/coremail/conf/hosts.cf
+HOSTS=/etc/hosts
+
+grep -E "\[|IP" $CMHOSTS | xargs -n2 | awk '{print $2" "$1}' | sed 's/IP=//;s/\[//;s/\]//' >> $HOSTS
+
+
