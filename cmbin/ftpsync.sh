12da7e05d6eefd8d0428833abe6347a5c0899e0d
Danielfeng <danielfancy@gmail.com>
2012-11-20 12:41:48 +0800

    ftp sync update

--- cmbin/ftpsync.sh
+++ cmbin/ftpsync.sh
@@ -5,14 +5,16 @@
 USER=用户名
 PASSWORD=密码
 IP=地址
-RELEASE=/var/share/release/
-LOGS=/home/ctools/logs/
-DATE=`date +"%Y%m%d"`
+RELEASE=/var/share/
+LOGS=/home/ctools/logs
+DATE=`date +"%Y-%m-%d"`
 
 [ -d $LOGS ] || mkdir -p $LOGS
-wget -r -b ftp://$USER:$PASSWORD@$IP/release/ -P $RELEASE -nH --cut-dirs=2 -o $LOGS/release.$DATE.log 
+wget -nH -m ftp://$USER:$PASSWORD@$IP/release/ -P $RELEASE -o $LOGS/release.$DATE.log 
 
-for i in `find ${LOGS} -type f -mtime +6 -name '*.*.log'` ; do
+##wget -r -b ftp://$USER:$PASSWORD@$IP/release/ -P $RELEASE -nH --cut-dirs=2 -o $LOGS/release.$DATE.log 
+
+for i in `find ${LOGS} -type f -mtime +6 -name 'release.*.log'` ; do
     if [ ! -z $i ]; then
       rm -f $i
     fi  
