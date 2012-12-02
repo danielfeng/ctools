f4d5b84d7d406129297ed5e3bb5e9b9d84a1c2b0
Danielfeng <danielfancy@gmail.com>
2012-12-02 16:52:26 +0800

    add init git install script

---
+++ sbin/init_git.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Author : danielfeng
+# E-Mail : danielfancy@gmail.com
+
+rpm -q curl             ||  yum -y install curl
+rpm -q crul-devel       ||  yum -y install curl-devel
+rpm -q zlib-devel       ||  yum -y install zlib-devel
+rpm -q openssl-devel    ||  yum -y install openssl-devel
+rpm -q perl             ||  yum -y install perl
+rpm -q cpio             ||  yum -y install cpio
+rpm -q expat-devel      ||  yum -y install expat-devel
+rpm -q gettext-devel    ||  yum -y install gettext-devel
+rpm -q gcc              ||  yum -y install gcc
+
+URL=http://www.codemonkey.org.uk/projects/git-snapshots/git/git-2012-12-02.tar.gz
+filename=`basename ${URL}`
+dirname=`echo ${filename} | sed 's/.tar.gz//g'`
+
+if [ -d /usr/local/src/${dirname} ]; then
+   rm -rf /usr/local/src/${dirname} 
+   mkdir -p /usr/local/src/${dirname}
+else
+   mkdir -p /usr/local/src/${dirname}
+fi
+
+wget -c -P /usr/local/src/ ${URL}
+tar xzvf /usr/local/src/${filename} -C /usr/local/src/
+cd /usr/local/src/${dirname}
+make && make install
