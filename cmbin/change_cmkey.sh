eb22d88f40f8abc8953a0058936ea640b8c7b33c
Danielfeng <danielfancy@gmail.com>
2012-12-06 15:03:15 +0800

    add batch change coremail key

---
+++ cmbin/change_cmkey.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Author : danielfeng
+# E-mail : danielfancy@gmail.com
+
+
+FINDGLOBAL=`find /home/coremail* -name "global.cf"`
+NLICENSE="License=\"$1\""
+
+if [[ -z $1 ]] ; then
+    echo "Usage: $0 ***== input new license"
+    exit
+fi
+
+for i in $FINDGLOBAL ; do
+    LICENSE=`grep "^License=" $i`
+    sed -i "s#${LICENSE}#${NLICENSE}#g" $i
+done
+
