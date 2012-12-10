b6979150a06e4329bca00857c1792f4efb4da24f
Danielfeng <danielfancy@gmail.com>
2012-12-10 22:38:30 +0800

    update install_coremail.sh fix dirname and  not null

--- cmbin/install_coremail.sh
+++ cmbin/install_coremail.sh
@@ -9,27 +9,28 @@ BIT=`echo $PACKAGE | awk -F "_" '{print $6}' | sed 's/.install.sh//g'`
 CMVER=`/home/coremail/libexec/udsvr -v | head -1 | awk -F "(" '{print $1}' | awk '{print $3}' | sed 's/\.//g'`
 DATE=`date +%Y%m%d`
 
+[[ -z ${PACKAGE} ]] && echo "Usage: $0 coremail install package name" && exit
+
 if [[ -L ${COREMAIL_HOME} ]] && [[ ! -z ${CMPROC} ]]; then
 	${COREMAIL_HOME}/sbin/cmctrl.sh stop
 	\rm ${COREMAIL_HOME}
 	sh ${PACKAGE}
 	${COREMAIL_HOME}/sbin/cmctrl.sh stop
-	mv ${COREMAIL_HOME} ${VER}x${BIT}_${DATE}
-	ln -nsf ${VER}x${BIT}_${DATE} ${COREMAIL_HOME}
+	mv ${COREMAIL_HOME} coremail${VER}x${BIT}_${DATE}
+	ln -nsf coremail${VER}x${BIT}_${DATE} ${COREMAIL_HOME}
 	${COREMAIL_HOME}/sbin/cmctrl.sh start
 elif [[ -d ${COREMAIL_HOME} ]] && [[ ! -z ${CMPROC} ]]; then
 	${COREMAIL_HOME}/sbin/cmctrl.sh stop
-	mv ${COREMAIL_HOME} ${CMVER}_${DATE} 
+	mv ${COREMAIL_HOME} coremail${CMVER}_${DATE} 
 	sh ${PACKAGE}
 	${COREMAIL_HOME}/sbin/cmctrl.sh stop
-	mv ${COREMAIL_HOME} ${VER}x${BIT}_${DATE}
-	ln -nsf ${VER}x${BIT}_${DATE} ${COREMAIL_HOME}
+	mv ${COREMAIL_HOME} coremail${VER}x${BIT}_${DATE}
+	ln -nsf coremail${VER}x${BIT}_${DATE} ${COREMAIL_HOME}
 	${COREMAIL_HOME}/sbin/cmctrl.sh start
 else
 	sh ${PACKAGE}
 	${COREMAIL_HOME}/sbin/cmctrl.sh stop
-	mv ${COREMAIL_HOME} ${VER}x${BIT}_${DATE}
-	ln -nsf ${VER}x${BIT}_${DATE} ${COREMAIL_HOME}
+	mv ${COREMAIL_HOME} coremail${VER}x${BIT}_${DATE}
+	ln -nsf coremail${VER}x${BIT}_${DATE} ${COREMAIL_HOME}
 	${COREMAIL_HOME}/sbin/cmctrl.sh start
 fi
-
