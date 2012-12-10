3e0402f518da4ed87dfb7278062a1b89d7d2bbc1
Danielfeng <danielfancy@gmail.com>
2012-12-10 23:39:40 +0800

    update install_coremail.sh fix dirname

--- cmbin/install_coremail.sh
+++ cmbin/install_coremail.sh
@@ -6,31 +6,38 @@ PACKAGE=$1
 CMPROC=`ps aux | grep coremail | grep "/home/coremail/bin/coremail" | grep -v grep`
 VER=`echo ${PACKAGE:2} | awk -F "_" '{print $1}' |sed 's/\.//g'`
 BIT=`echo $PACKAGE | awk -F "_" '{print $6}' | sed 's/.install.sh//g'`
-CMVER=`/home/coremail/libexec/udsvr -v | head -1 | awk -F "(" '{print $1}' | awk '{print $3}' | sed 's/\.//g'`
 DATE=`date +%Y%m%d`
 
 [[ -z ${PACKAGE} ]] && echo "Usage: $0 coremail install package name" && exit
 
-if [[ -L ${COREMAIL_HOME} ]] && [[ ! -z ${CMPROC} ]]; then
-	${COREMAIL_HOME}/sbin/cmctrl.sh stop
+if [[ -f ${COREMAIL_HOME}/libexec/udsvr ]] ; then
+   CMVER=`${COREMAIL_HOME}/libexec/udsvr -v | head -1 | awk -F "(" '{print $1}' | awk '{print $3}' | sed 's/\.//g'`
+fi
+
+if [[ -L ${COREMAIL_HOME} ]]; then
+    if [[ ! -z ${CMPROC} ]] ; then
+	    ${COREMAIL_HOME}/sbin/cmctrl.sh stop
+    fi
 	\rm ${COREMAIL_HOME}
 	sh ${PACKAGE}
 	${COREMAIL_HOME}/sbin/cmctrl.sh stop
-	mv ${COREMAIL_HOME} coremail${VER}x${BIT}_${DATE}
-	ln -nsf coremail${VER}x${BIT}_${DATE} ${COREMAIL_HOME}
+	mv ${COREMAIL_HOME} coremail${VER}_x${BIT}_${DATE}
+	ln -nsf coremail${VER}_x${BIT}_${DATE} ${COREMAIL_HOME}
 	${COREMAIL_HOME}/sbin/cmctrl.sh start
-elif [[ -d ${COREMAIL_HOME} ]] && [[ ! -z ${CMPROC} ]]; then
-	${COREMAIL_HOME}/sbin/cmctrl.sh stop
-	mv ${COREMAIL_HOME} coremail${CMVER}_${DATE} 
+elif [[ -d ${COREMAIL_HOME} ]]; then
+    if [[ ! -z ${CMPROC} ]] ; then
+	    ${COREMAIL_HOME}/sbin/cmctrl.sh stop
+    fi
+	mv ${COREMAIL_HOME} coremail${CMVER}_${DATE}_dn 
 	sh ${PACKAGE}
 	${COREMAIL_HOME}/sbin/cmctrl.sh stop
-	mv ${COREMAIL_HOME} coremail${VER}x${BIT}_${DATE}
-	ln -nsf coremail${VER}x${BIT}_${DATE} ${COREMAIL_HOME}
+	mv ${COREMAIL_HOME} coremail${VER}_x${BIT}_${DATE}
+	ln -nsf coremail${VER}_x${BIT}_${DATE} ${COREMAIL_HOME}
 	${COREMAIL_HOME}/sbin/cmctrl.sh start
 else
 	sh ${PACKAGE}
 	${COREMAIL_HOME}/sbin/cmctrl.sh stop
-	mv ${COREMAIL_HOME} coremail${VER}x${BIT}_${DATE}
-	ln -nsf coremail${VER}x${BIT}_${DATE} ${COREMAIL_HOME}
+	mv ${COREMAIL_HOME} coremail${VER}_x${BIT}_${DATE}
+	ln -nsf coremail${VER}_x${BIT}_${DATE} ${COREMAIL_HOME}
 	${COREMAIL_HOME}/sbin/cmctrl.sh start
 fi
