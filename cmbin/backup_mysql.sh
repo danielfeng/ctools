b419d5b6a498c961cd0411596196c4b882217cf1
Danielfeng <danielfancy@gmail.com>
2012-12-12 19:51:03 +0800

    update backup_mysql.sh different dump

--- cmbin/backup_mysql.sh
+++ cmbin/backup_mysql.sh
@@ -6,6 +6,8 @@ HOSTNAME=$(grep cm_logs_db /home/coremail/conf/datasources.cf -5 | grep Server |
 USERNAME=$(grep cm_logs_db /home/coremail/conf/datasources.cf -5 | grep User|awk -F\" '{print $2}')
 PASSWORD=$(grep cm_logs_db /home/coremail/conf/datasources.cf -5 | grep Password |awk -F\" '{print $2}')
 PORT=$(grep cm_logs_db /home/coremail/conf/datasources.cf -5 | grep Port |awk -F\" '{print $2}')
+ARRAY=$(grep "Database=" /home/coremail/conf/datasources.cf | awk -F\" '{print $2}' | sort | uniq)
+DATE=$(date +%Y%m%d)
 
 MYSQLBAK_LOG="/home/coremail/logs/backup_mysql.log"
 MYSQLDUMP="/home/coremail/mysql/bin/mysqldump"
@@ -16,5 +18,6 @@ if [ ! -d ${BAK_DIR} ] ; then
     mkdir -p ${BAK_DIR}
 fi
 
-
-$MYSQLDUMP -A -u${USERNAME} -p${PASSWORD} -h${HOSTNAME} -P${PORT} > ${BAK_DIR}/all_database.sql #>> ${MYSQLBAK_LOG}
+for i in $ARRAY; do
+    $MYSQLDUMP -u${USERNAME} -p${PASSWORD} -h${HOSTNAME} -P${PORT} --single-transaction --default-character-set=gbk ${i} > ${BAK_DIR}/${i}_${DATE}.sql 
+done
