6e43ef7dd96a5ecb52085897db3559552a80ce34
Danielfeng <danielfancy@gmail.com>
2012-12-16 19:41:04 +0800

    update install_milti_cm.sh set rsa.pub

--- cmbin/install_milti_cm.sh
+++ cmbin/install_milti_cm.sh
@@ -2,9 +2,13 @@
 # Author : danielfeng
 # E-mail : danielfancy@gmail.com
 
-H
+CMDIR=/home/ctools/cmconf
+LOCAL_IP=`/sbin/ifconfig -a | awk '/inet/{print $2}' | awk -F: '{print $2}' | grep -v "127.0.0.1" | grep -v '^$' | sort`
+CMIP=`grep ${CMDIR}/hosts.cf | xargs -n2 | awk '{print $2" "$1}' | sed 's/IP=//;s/\[//;s/\]//' | awk '{print $1}'` 
+CMHOSTS=`grep ${CMDIR}/hosts.cf | xargs -n2 | awk '{print $2" "$1}' | sed 's/IP=//;s/\[//;s/\]//' | awk '{print $2}'` 
 
-
-for rsa in words; do
-	#statements
+for rsa in `grep -v ${LOCAL_IP} ${CMIP}; do
+	ssh-copy-id -i ${CMDIR}/.coremail.pub root@${rsa}
 done
+
+echo "ok"
