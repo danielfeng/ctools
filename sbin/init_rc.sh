37bfd8bb5f8f31dfff4d86520aac7e6d4e3931ce
Danielfeng <danielfancy@gmail.com>
2012-12-17 12:59:33 +0800

    update init_rc.sh use for() stop and off system service

--- sbin/init_rc.sh
+++ sbin/init_rc.sh
@@ -1,52 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Author : danielfeng
 # E-Mail : danielfancy@gmail.com
 
-service sendmail stop
-service iptables stop
+service_stop_list="sendmail iptables cups nfslock bluetooth hidd gpm ip6tables pcscd xfs autofs avahi-daemon auditd rpcidmapd rpcgssd isdn httpd"
 
-chkconfig sendmail off
-chkconfig iptables off
+for list in ${service_stop_list}; do
+    [[ ! -f /etc/init.d/${list} ]] && continue
+    LANG=C service ${list} stop
+    LANG=C chkconfig ${list} off
+done
 
-
-#chkconfig portmap off     	#NIS 和NFS 提供动态端口的分配
-chkconfig cups off       	#Common UNIX Printing System
-chkconfig nfslock off		#NFS file lock
-#chkconfig apmd off		#Advanced Power Management
-chkconfig bluetooth off		
-chkconfig hidd off		#Bluetooth H.I.D service
-chkconfig gpm off		#General Purpose Mouse Daemon
-chkconfig ip6tables off		
-chkconfig pcscd off		#读卡器	
-chkconfig xfs off		#x font server
-#chkconfig netfs off			
-chkconfig autofs off		#cd auto
-chkconfig avahi-daemon off      #发现基于zeroconf协议的设备
-chkconfig auditd off		#审核守护进程
-#chkconfig haldaemon off
-chkconfig rpcidmapd off		#redhat update software
-chkconfig rpcgssd   off		#redhat update software
-chkconfig isdn off		#isdn modem
-chkconfig httpd off
-
-
-
-service httpd stop
-#service portmap stop
-service cups stop 
-service nfslock stop
-#service apmd stop
-service bluetooth stop
-service hidd stop
-service gpm stop
-service ip6tables stop
-service pcscd stop
-service xfs stop
-#service netfs stop
-service autofs stop
-service avahi-daemon stop
-service auditd stop
-#service haldaemon stop
-service rpcidmapd stop
-service rpcgssd stop
-service isdn stop
+## portmap      	#NIS 和NFS 提供动态端口的分配
+#  cups        		#Common UNIX Printing System
+#  nfslock 			#NFS file lock
+## apmd 			#Advanced Power Management
+#  hidd 			#Bluetooth H.I.D service
+#  gpm 				#General Purpose Mouse Daemon
+#  pcscd 			#读卡器	
+#  xfs 				#x font server
+#  autofs 			#cd auto
+#  avahi-daemon     #发现基于zeroconf协议的设备
+#  auditd 			#审核守护进程
+#  rpcidmapd 		#redhat update software
+#  rpcgssd   		#redhat update software
+#  isdn 			#isdn modem
