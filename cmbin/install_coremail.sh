f6d8d5623fb6b4178451be29079404f07e4b154d
Danielfeng <danielfancy@gmail.com>
2012-12-21 22:49:24 +0800

    update install_coremail.sh rebuild and fix path

--- cmbin/install_coremail.sh
+++ cmbin/install_coremail.sh
@@ -4,16 +4,43 @@
 
 PACKAGE=$1
 CMPROC=`ps aux | grep coremail | grep "/home/coremail/bin/coremail" | grep -v grep`
-VER=`echo ${PACKAGE:2} | awk -F "_" '{print $1}' |sed 's/\.//g'`
-BIT=`echo $PACKAGE | awk -F "_" '{print $6}' | sed 's/.install.sh//g'`
 DATE=`date +%Y%m%d`
 TIME=`date +%H%M`
-PACKAGENAME=`grep "install.sh" ${PACKAGE}`
+PACKAGENAME=`basename ${PACKAGE} | grep "install.sh"`
+VER=`echo ${PACKAGENAME:2} | awk -F "_" '{print $1}' |sed 's/\.//g'`
+BIT=`echo $PACKAGENAME | awk -F "_" '{print $6}' | sed 's/.install.sh//g'`
 
 [[ -z ${PACKAGE} ]] || [[ -z ${PACKAGENAME} ]] && echo "Usage: $0 coremail install package name" && exit
 
 if [[ -f ${COREMAIL_HOME}/libexec/udsvr ]] ; then
    CMVER=`${COREMAIL_HOME}/libexec/udsvr -v | head -1 | awk -F "(" '{print $1}' | awk '{print $3}' | sed 's/\.//g'`
+fi
+
+install_coremail(){
+    sh ${PACKAGE}
+    ${COREMAIL_HOME}/sbin/cmctrl.sh stop
+    if [[ -d /home/coremail${VER}_x${BIT}_${DATE} ]] ; then
+        mv /home/coremail${VER}_x${BIT}_${DATE}{,_bak_${TIME}}
+    fi
+    mv ${COREMAIL_HOME} /home/coremail${VER}_x${BIT}_${DATE}
+    ln -nsf /home/coremail${VER}_x${BIT}_${DATE} ${COREMAIL_HOME}
+    ${COREMAIL_HOME}/sbin/cmctrl.sh start
+}
+
+if [[ -L ${COREMAIL_HOME} ]]; then
+    if [[ ! -z ${CMPROC} ]] ; then
+	    ${COREMAIL_HOME}/sbin/cmctrl.sh stop
+    fi
+	\rm ${COREMAIL_HOME}
+    install_coremail
+elif [[ -d ${COREMAIL_HOME} ]]; then
+    if [[ ! -z ${CMPROC} ]] ; then
+	    ${COREMAIL_HOME}/sbin/cmctrl.sh stop
+    fi
+	mv ${COREMAIL_HOME} /home/coremail${CMVER}_${DATE}_dn 
+    install_coremail
+else
+    install_coremail
 fi
 
 install_url() {
@@ -22,43 +49,4 @@ install_url() {
     echo "Unpack OK! Browse the URL below to configure coremail:"
     echo "      http://${LOCAL_IP}${WEBINSTPATH}"
 }
-
-if [[ -L ${COREMAIL_HOME} ]]; then
-    if [[ ! -z ${CMPROC} ]] ; then
-	    ${COREMAIL_HOME}/sbin/cmctrl.sh stop
-    fi
-	\rm ${COREMAIL_HOME}
-	sh ${PACKAGE}
-	${COREMAIL_HOME}/sbin/cmctrl.sh stop
-    if [[ -d coremail${VER}_x${BIT}_${DATE} ]] ; then
-        mv coremail${VER}_x${BIT}_${DATE}{,_bak_${TIME}}
-    fi
-	mv ${COREMAIL_HOME} coremail${VER}_x${BIT}_${DATE}
-	ln -nsf coremail${VER}_x${BIT}_${DATE} ${COREMAIL_HOME}
-	${COREMAIL_HOME}/sbin/cmctrl.sh start
-    install_url
-elif [[ -d ${COREMAIL_HOME} ]]; then
-    if [[ ! -z ${CMPROC} ]] ; then
-	    ${COREMAIL_HOME}/sbin/cmctrl.sh stop
-    fi
-	mv ${COREMAIL_HOME} coremail${CMVER}_${DATE}_dn 
-	sh ${PACKAGE}
-	${COREMAIL_HOME}/sbin/cmctrl.sh stop
-    if [[ -d coremail${VER}_x${BIT}_${DATE} ]] ; then
-        mv coremail${VER}_x${BIT}_${DATE}{,_bak_${TIME}}
-    fi
-	mv ${COREMAIL_HOME} coremail${VER}_x${BIT}_${DATE}
-	ln -nsf coremail${VER}_x${BIT}_${DATE} ${COREMAIL_HOME}
-	${COREMAIL_HOME}/sbin/cmctrl.sh start
-    install_url
-else
-	sh ${PACKAGE}
-	${COREMAIL_HOME}/sbin/cmctrl.sh stop
-    if [[ -d coremail${VER}_x${BIT}_${DATE} ]] ; then
-        mv coremail${VER}_x${BIT}_${DATE}{,_bak_${TIME}}
-    fi
-	mv ${COREMAIL_HOME} coremail${VER}_x${BIT}_${DATE}
-	ln -nsf coremail${VER}_x${BIT}_${DATE} ${COREMAIL_HOME}
-	${COREMAIL_HOME}/sbin/cmctrl.sh start
-    install_url
-fi
+install_url
