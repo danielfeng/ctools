35e14a6fcf123f3baeffa9b598c7f25a214a7383
Danielfeng <danielfancy@gmail.com>
2012-12-22 15:22:25 +0800

    update boot_coremail.sh chk_cmsvr way

--- cmbin/boot_coremail.sh
+++ cmbin/boot_coremail.sh
@@ -4,9 +4,19 @@
 
 COREMAIL_HOME=/home/coremail
 
-cp $COREMAIL_HOME/sbin/cmctrl.sh /etc/init.d/coremail  || exit 1
-chmod a+x /etc/init.d/coremail  || exit 1
-echo "Checking runlevel information for coremail services"
-/sbin/chkconfig --add coremail  || exit 1
-/sbin/chkconfig --list coremail
+chk_cmsvr(){
+	/sbin/chkconfig --add mtcoremail || exit 1
+	/sbin/chkconfig --list mtcoremail
+}
+
+if [[ ! -f /etc/init.d/mtcoremail ]]; then
+	cp $COREMAIL_HOME/sbin/cmctrl.sh /etc/init.d/mtcoremail  || exit 1
+	chmod a+x /etc/init.d/mtcoremail  || exit 1
+	echo "Checking runlevel information for coremail services"
+	chk_cmsvr
+else
+	chk_cmsvr
+	echo "Added runlevel information for coremail services"
+fi
+
 
