c6b9ace383f1f3e4ff43e08a8ded5451d2f31c7d
Danielfeng <danielfancy@gmail.com>
2012-12-22 16:36:37 +0800

    update add_cmhosts.sh whether IP_HOSTS add hosts

--- cmbin/add_cmhosts.sh
+++ cmbin/add_cmhosts.sh
@@ -2,9 +2,11 @@
 # Author : danielfeng
 # E-Mail : danielfancy@gmail.com
 
-CMHOSTS=${COREMAIL_HOME}/conf/hosts.cf
+CMHOSTS="${COREMAIL_HOME}/conf/hosts.cf"
 HOSTS=/etc/hosts
+HOSTS_IP=`grep -E "\[|IP" $CMHOSTS | xargs -n2 | awk '{print $2":"$1}' | sed 's/IP=//;s/\[//;s/\]//'`
 
-grep -E "\[|IP" $CMHOSTS | xargs -n2 | awk '{print $2" "$1}' | sed 's/IP=//;s/\[//;s/\]//' >> $HOSTS
-
-
+for i in ${HOSTS_IP[@]} ; do
+    _HOSTSIP_=`grep "${i##*:}" ${HOSTS}`
+    [[ -z ${_HOSTSIP_} ]] && echo "${i%%:*} ${i##*:}" >> ${HOSTS}
+done
