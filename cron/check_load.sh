253b1917701e20f148e920c3746f32e0358338de
Danielfeng <danielfancy@gmail.com>
2013-02-19 17:25:22 +0800

    update check_load mail -s way

--- cron/check_load.sh
+++ cron/check_load.sh
@@ -2,16 +2,17 @@
 # Author : danielfeng
 # E-Mail : danielfancy@gmail.com
 
-LOGS=/home/ctools/logs/
 DATE=`date +"%Y-%m-%d"`
 LOAD=$(awk '{print $1}' /proc/loadavg)
 CPUNUM=$(grep -c processor /proc/cpuinfo)
+TMP_LOAD=/tmp/loadavg.tmp
 
-[ -d $LOGS ] || mkdir -p $LOGS
+[[ -f ${TMP_LOAD} ]] && \rm ${TMP_LOAD}  
 
 if [ $(echo "$LOAD > $CPUNUM" | bc) = 1 ]; then
     RESULT=$(ps -eo pcpu,pmem,user,args | awk '$1 > 0' | sort -nr)
     if [ -n "$RESULT" ]; then
-        echo "$RESULT" > ${LOGS}/ps.log.${DATE}
+        echo "$RESULT" >  ${TMP_LOAD}
+        mail -s "Report Coremail mail system check loadavg from `hostname` ${DATA}" ddiao@mailtech.cn < ${TMP_LOAD}
     fi
 fi
