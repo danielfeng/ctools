e900efdefd5ea1b69b86760b28afa8c0a684d75f
Danielfeng <danielfancy@gmail.com>
2013-02-20 16:49:10 +0800

    update check_load mail -f way

--- cron/check_load.sh
+++ cron/check_load.sh
@@ -12,7 +12,8 @@ TMP_LOAD=/tmp/loadavg.tmp
 if [ $(echo "$LOAD > $CPUNUM" | bc) = 1 ]; then
     RESULT=$(ps -eo pcpu,pmem,user,args | awk '$1 > 0' | sort -nr)
     if [ -n "$RESULT" ]; then
-        echo "$RESULT" >  ${TMP_LOAD}
-        mail -s "Report Coremail mail system check loadavg from `hostname` ${DATA}" ddiao@mailtech.cn < ${TMP_LOAD}
+        w > ${TMP_LOAD}
+        echo "$RESULT" >>  ${TMP_LOAD}
+        mail -s "Report Coremail mail system check loadavg from `hostname` ${DATA}" ddiao@mailtech.cn < ${TMP_LOAD} -- -f root@`hostname`.com
     fi
 fi
