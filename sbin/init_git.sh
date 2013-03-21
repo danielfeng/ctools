ea71290af3e21856913a2d6503d0f498368c46f8
Danielfeng <danielfancy@gmail.com>
2013-03-21 13:06:43 +0800

    update init_git.sh yum perl-ExtUtils-MakeMaker

--- sbin/init_git.sh
+++ sbin/init_git.sh
@@ -2,16 +2,17 @@
 # Author : danielfeng
 # E-Mail : danielfancy@gmail.com
 
-rpm -q curl             ||  yum -y install curl
-rpm -q crul-devel       ||  yum -y install curl-devel
-rpm -q zlib-devel       ||  yum -y install zlib-devel
-rpm -q openssl-devel    ||  yum -y install openssl-devel
-rpm -q perl             ||  yum -y install perl
-rpm -q cpio             ||  yum -y install cpio
-rpm -q expat-devel      ||  yum -y install expat-devel
-rpm -q gettext-devel    ||  yum -y install gettext-devel
-rpm -q gcc              ||  yum -y install gcc
-rpm -q autoconf         ||  yum -y install autoconf
+rpm -q curl                     ||  yum -y install curl
+rpm -q crul-devel               ||  yum -y install curl-devel
+rpm -q zlib-devel               ||  yum -y install zlib-devel
+rpm -q openssl-devel            ||  yum -y install openssl-devel
+rpm -q perl                     ||  yum -y install perl
+rpm -q cpio                     ||  yum -y install cpio
+rpm -q expat-devel              ||  yum -y install expat-devel
+rpm -q gettext-devel            ||  yum -y install gettext-devel
+rpm -q gcc                      ||  yum -y install gcc
+rpm -q autoconf                 ||  yum -y install autoconf
+rpm -q perl-ExtUtils-MakeMaker  ||  yum -y install perl-ExtUtils-MakeMaker
 
 URL=http://www.codemonkey.org.uk/projects/git-snapshots/git/git-2012-12-02.tar.gz
 filename=`basename ${URL}`
