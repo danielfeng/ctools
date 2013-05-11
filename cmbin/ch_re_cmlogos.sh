52aaadb6b64282c59aa64e881a0e1e6a53e51b9b
Danielfeng <danielfancy@gmail.com>
2013-05-11 17:13:38 +0800

    update ch_re_cmlogos.sh PATH

--- cmbin/ch_re_cmlogos.sh
+++ cmbin/ch_re_cmlogos.sh
@@ -3,27 +3,27 @@
 # E-mail : danielfancy@gmail.com
 
 change_logos_list=(
-    "/home/ctools/cmconf/cm_logos/cmxt3/1.gif:/home/coremail/web/webapp/common/index_cmxt30/logo.gif" \
-    "/home/ctools/cmconf/cm_logos/cmxt3/2.jpg:/home/coremail/web/webapp/common/index_cmxt30/mainBg1.jpg" \
-    "/home/ctools/cmconf/cm_logos/cmxt3/3.jpg:/home/coremail/web/webapp/common/index_cmxt30/mainBg.jpg" \
-    "/home/ctools/cmconf/cm_logos/cmxt3/4.gif:/home/coremail/web/webapp/XT3/assets/21389/images/logo.gif" \
-    "/home/ctools/cmconf/cm_logos/cmxt3/5.jpg:/home/coremail/web/webapp/XT3/assets/21389/images/t_tr_blue.jpg" \
-    "/home/ctools/cmconf/cm_logos/cmxt3/6.jpg:/home/coremail/web/webapp/common/assets/21389/help/images/help_logo.jpg" \
-    "/home/ctools/cmconf/cm_logos/cmxt3/7.jpg:/home/coremail/web/webapp/common/assets/21389/help/images/helplogo_zh.jpg" \
-    "/home/ctools/cmconf/cm_logos/cmxt3/8.gif:/home/coremail/web/webapp/ipad/assets/logo/logo.gif" \
-    "/home/ctools/cmconf/cm_logos/cmxt3/9.jpg:/home/coremail/web/webapp/ipad/assets/xm_index/mainBg.jpg" \
-    "/home/ctools/cmconf/cm_logos/cmxt3/10.png:/home/coremail/web/webapp/ipad/assets/xm_index/mainBg_1.png" \
-    "/home/ctools/cmconf/cm_logos/cmxt3/11.png:/home/coremail/web/webapp/ipad/assets/xm_index/mainBg_2.png" \
-    "/home/ctools/cmconf/cm_logos/cmxt3/12.png:/home/coremail/web/webapp/ipad/assets/xm_index/mainBg_3.png" \
-    "/home/ctools/cmconf/cm_logos/cmxt3/13.gif:/home/coremail/web/webapp/ipad/assets/logo/logo-s.gif" \
-    "/home/ctools/cmconf/cm_logos/cmxt3/14.png:/home/coremail/web/webapp/xphone/assets/logo/logo.png" \
-    "/home/ctools/cmconf/cm_logos/cmxt3/15.ico:/home/coremail/web/html/favicon.ico" \
+    "/home/ctools/cmconf/cm_logos/cmxt3/1.gif:${COREMAIL_HOME}/web/webapp/common/index_cmxt30/logo.gif" \
+    "/home/ctools/cmconf/cm_logos/cmxt3/2.jpg:${COREMAIL_HOME}/web/webapp/common/index_cmxt30/mainBg1.jpg" \
+    "/home/ctools/cmconf/cm_logos/cmxt3/3.jpg:${COREMAIL_HOME}/web/webapp/common/index_cmxt30/mainBg.jpg" \
+    "/home/ctools/cmconf/cm_logos/cmxt3/4.gif:${COREMAIL_HOME}/web/webapp/XT3/assets/21389/images/logo.gif" \
+    "/home/ctools/cmconf/cm_logos/cmxt3/5.jpg:${COREMAIL_HOME}/web/webapp/XT3/assets/21389/images/t_tr_blue.jpg" \
+    "/home/ctools/cmconf/cm_logos/cmxt3/6.jpg:${COREMAIL_HOME}/web/webapp/common/assets/21389/help/images/help_logo.jpg" \
+    "/home/ctools/cmconf/cm_logos/cmxt3/7.jpg:${COREMAIL_HOME}/web/webapp/common/assets/21389/help/images/helplogo_zh.jpg" \
+    "/home/ctools/cmconf/cm_logos/cmxt3/8.gif:${COREMAIL_HOME}/web/webapp/ipad/assets/logo/logo.gif" \
+    "/home/ctools/cmconf/cm_logos/cmxt3/9.jpg:${COREMAIL_HOME}/web/webapp/ipad/assets/xm_index/mainBg.jpg" \
+    "/home/ctools/cmconf/cm_logos/cmxt3/10.png:${COREMAIL_HOME}/web/webapp/ipad/assets/xm_index/mainBg_1.png" \
+    "/home/ctools/cmconf/cm_logos/cmxt3/11.png:${COREMAIL_HOME}/web/webapp/ipad/assets/xm_index/mainBg_2.png" \
+    "/home/ctools/cmconf/cm_logos/cmxt3/12.png:${COREMAIL_HOME}/web/webapp/ipad/assets/xm_index/mainBg_3.png" \
+    "/home/ctools/cmconf/cm_logos/cmxt3/13.gif:${COREMAIL_HOME}/web/webapp/ipad/assets/logo/logo-s.gif" \
+    "/home/ctools/cmconf/cm_logos/cmxt3/14.png:${COREMAIL_HOME}/web/webapp/xphone/assets/logo/logo.png" \
+    "/home/ctools/cmconf/cm_logos/cmxt3/15.ico:${COREMAIL_HOME}/web/html/favicon.ico" \
 )
 
 restart_clean_tomcat()
 {
-    rm -rf /home/coremail/java/tomcat/work/*
-    /home/coremail/bin/coremail restart tomcat
+    rm -rf ${COREMAIL_HOME}/java/tomcat/work/*
+    ${COREMAIL_HOME}/bin/coremail restart tomcat
 }
 
 change_logos()
@@ -45,7 +45,6 @@ restore_logos()
     done
     restart_clean_tomcat
 }
-
 
 case $1 in
 restore | -r )
