4939cf5d731a66afa0670d213884346b68721644
Danielfeng <danielfancy@gmail.com>
2013-06-14 01:02:34 +0800

    update init_ntp.sh fix ctools path

--- sbin/init_ntp.sh
+++ sbin/init_ntp.sh
@@ -2,10 +2,13 @@
 # Author : danielfeng
 # E-Mail : danielfancy@gmail.com
 
-. ${CTOOLS}/functions
+CTOOLS=/home/ctools
 NTPDCONF=/etc/sysconfig/ntpd
-SYNC_HWCLOCK=no
+. ${CTOOLS}/functions
+#SYNC_HWCLOCK=no
 
 i_need_yum ntp
+srv_stop ntp
+ntpdate time.windows.com && hwclock -w
 grep -q "SYNC_HWCLOCK=no" ${NTPDCONF} || sed -i "s#SYNC_HWCLOCK=no#SYNC_HWCLOCK=yes#g" ${NTPDCONF}
-srv_restart ntpd
+srv_start ntpd
