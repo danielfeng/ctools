47cfaf45c55b5c8855039d88a64c8ed1d0b092ac
Danielfeng <danielfancy@gmail.com>
2013-07-04 11:08:23 +0800

    update change_cmkey.sh echo changed file

--- cmbin/change_cmkey.sh
+++ cmbin/change_cmkey.sh
@@ -3,8 +3,8 @@
 # E-mail : danielfancy@gmail.com
 
 
-#FINDGLOBAL=`find /home/coremail* -name "global.cf"`
-FINDGLOBAL=`locate global.cf | grep -v ".tpl"`
+FINDGLOBAL=`find /home/coremail* -name "global.cf"`
+#FINDGLOBAL=`locate global.cf | grep -v ".tpl"`
 NLICENSE="License=\"$1\""
 
 if [[ -z $1 ]] ; then
@@ -15,5 +15,6 @@ fi
 for i in $FINDGLOBAL ; do
     LICENSE=`grep "^License=" $i`
     sed -i "s#${LICENSE}#${NLICENSE}#g" $i
+    echo $i
 done
 
