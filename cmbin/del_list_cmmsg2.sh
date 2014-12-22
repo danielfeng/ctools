36092e939cc026d8268a88a7a2986082eb5a1bb0
Danielfeng <danielfancy@gmail.com>
2014-12-22 18:14:55 +0800

    fix del list unread all msg

--- cmbin/del_list_cmmsg2.sh
+++ cmbin/del_list_cmmsg2.sh
@@ -2,37 +2,63 @@
 # Author : danielfeng
 # E-mail : danielfancy@gmail.com
 
-KEYWORD=$3
-SEND_USER=$2
+KEYWORD=$2
 
-[[ -z ${KEYWORD} ]] && echo "Usage: $0 -l list email_id keyword msginfo| -d del email_id keyword need msg " && exit
-[[ -z ${SEND_USER} ]] && echo "Usage: $0 -l list email_id keyword msginfo| -d del email_id keyword need msg " && exit
+[[ -z ${KEYWORD} ]] && echo "Usage: $0 -la -lu list email_id keyword msginfo| -d del email_id keyword need msg " && exit
 
-
-USER_LIST=`/home/coremail/bin/userutil --select-user @`
+USERUTIL="/home/coremail/bin/userutil"
+USER_LIST=`${USERUTIL} --select-user @`
 
 list_unread_msginfo()
 {
-	for u in ${USER_LIST[@]} ; do 
-    	/home/coremail/bin/userutil --list-msg ${u} | grep -v "MBoxID:\|----------" | sed "s/^/${u} /g" | grep "\.\." | grep ${KEYWORD} |  awk '{if($9="'${SEND_USER}'")print $0}'  >> /tmp/unreadlog 
-	done
-	tail /tmp/unreadlog
+    for u in ${USER_LIST[@]} ; do 
+        ${USERUTIL} --list-msg ${u} | grep -v "MBoxID:\|----------" | sed "s/^/${u} /g" | grep "\.\." | awk '{for(i=9;i<NF;i++)if($i~/'${KEYWORD}'/)print $0}'  | sort | uniq >> /tmp/unreadlog 
+    done
+    cat /tmp/unreadlog && \rm /tmp/unreadlog
 }
 
-list_need_msginfo()
+list_all_msginfo()
 {
-	for u in ${USER_LIST[@]} ; do 
-    	/home/coremail/bin/userutil --list-msg ${u} | grep -v "MBoxID:\|----------" | sed "s/^/${u} /g" | grep ${KEYWORD} | awk '{if($9="'${SEND_USER}'")print $0}' >> /tmp/alllog 
-	done
-	tail /tmp/alllog
+    for u in ${USER_LIST[@]} ; do 
+        ${USERUTIL} --list-msg ${u} | grep -v "MBoxID:\|----------" | sed "s/^/${u} /g" | awk '{for(i=9;i<NF;i++)if($i~/'${KEYWORD}'/)print $0}'  | sort | uniq >> /tmp/alllog 
+    done
+    cat /tmp/alllog && \rm /tmp/alllog
 }
+
+del_unread_msginfo()
+{
+    for u in ${USER_LIST[@]} ; do 
+        ${USERUTIL} --list-msg ${u} | grep -v "MBoxID:\|----------" | sed "s/^/${u} /g" | grep "\.\." | awk '{for(i=9;i<NF;i++)if($i~/'${KEYWORD}'/)print $0}'  | sort | uniq >> /tmp/unreadlog 
+    done
+    for d in `cat /tmp/unreadlog | sort | uniq | awk '{print $1":"$3}'` ; do  
+        ${USERUTIL} --delete-msg ${d%%:*} ${d##*:}
+    done;
+    \rm /tmp/unreadlog
+}
+
+del_all_msginfo()
+{
+    for u in ${USER_LIST[@]} ; do 
+        ${USERUTIL} --list-msg ${u} | grep -v "MBoxID:\|----------" | sed "s/^/${u} /g" | awk '{for(i=9;i<NF;i++)if($i~/'${KEYWORD}'/)print $0}' | sort | uniq >> /tmp/alllog
+    done
+    for d in `cat /tmp/alllog  | sort | uniq | awk '{print $1":"$3}'` ; do  
+        ${USERUTIL} --delete-msg ${d%%:*} ${d##*:}
+    done;
+    \rm /tmp/alllog
+}
+
+
 
 case $1 in
 --list-all | -la )
-    list_unread_msginfo;;
+    list_all_msginfo;;
 --list-unread  | -lu )
-    list_need_msginfo;;
+    list_unread_msginfo;;
+--del-all | -da )
+    del_all_msginfo;;
+--del-unread  | -du )
+    del_unread_msginfo;;
 *)
-    echo "Usage: $0 -l list email_id keyword msginfo| -d del email_id keyword need msg ";;
+    echo "Usage: $0 -la or -lu list all or unread msg | -da or -du del all or unread msg && keyword need";;
 esac
 
