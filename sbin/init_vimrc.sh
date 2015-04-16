e9a44b6458417c8a7ed9c74927a26cd985a9e273
Danielfeng <danielfancy@gmail.com>
2015-04-16 21:48:58 +0800

    vimrc

--- sbin/init_vimrc.sh
+++ sbin/init_vimrc.sh
@@ -3,9 +3,15 @@
 # E-Mail : danielfancy@gmail.com
 
 CONF=/home/ctools/conf
+VIM=/home/ctools/conf/vim
 ETC=/etc
 
-
+if [ -d ~/.vim ]; then
+    cp -r ${VIM}/* ~/.vim
+else
+    mkdir ~/.vim
+    cp -r ${VIM}/* ~/.vim
+fi
 
 if [ -f ${ETC}/vimrc ] ; then
     mv ${ETC}/vimrc ${ETC}/vimrc.original
