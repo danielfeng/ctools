9b50f78bf461bdc665e5a44c0aa5ad5c0d86205f
Danielfeng <danielfancy@gmail.com>
2015-07-09 18:10:28 +0800

    chk coremail version

---
+++ cmbin/chk_cm.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Author : danielfeng
+# E-mail : danielfancy@gmail.com
+
+CM_HOME="/home/coremail"
+
+
+echo "查找coremail安装包"
+find /home/ /root/ -maxdepth 2 -name *.install.sh
+
+echo "操作系统版本"
+cat /etc/redhat-release
+
+echo "检查coremail程序位数"
+for x in `ls ${CM_HOME}/libexec/`; do file ${CM_HOME}/libexec/$x | awk -F "," '{print $1}';done
+
+echo "检查coremail程序版本"
+${CM_HOME}/bin/sautil --chkver --withC > /tmp/1 && for i in `ls ${CM_HOME}/libexec/`; do cat 1 | grep $i ; done && rm -f /tmp/1
