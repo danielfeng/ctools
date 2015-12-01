1c283b2f7da8d75a04c015625c00877497a83d1e
Danielfeng <danielfancy@gmail.com>
2015-12-01 17:04:11 +0800

    update cm_docker3 fix =

--- cron/cm_docker3.sh
+++ cron/cm_docker3.sh
@@ -8,7 +8,7 @@ docker rm `docker ps -a -q`
 /etc/init.d/docker restart
 
 docker run -p 81:3000 -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -i -t --name=dn_ct_rails dn_ct_rails bash -c "cd /home/app/webapp/ && rails s"
-docker run -p 82:3000 -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -i -t --name-dn_ct_rails5 dn_ct_rails5 bash -c "cd /home/app/webapp/ && rails s"
+docker run -p 82:3000 -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -i -t --name=dn_ct_rails5 dn_ct_rails5 bash -c "cd /home/app/webapp/ && rails s"
 
 docker run -d -p 80:80 -v /etc/nginx/:/etc/nginx/ -v /var/log/nginx:/var/log/nginx -v /etc/localtime:/etc/localtime nginx
 
