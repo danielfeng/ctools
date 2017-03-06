8044b6142c489eb771f2f76e8f1d319e32d801e4
danielfancy <danielfancy@gmail.com>
2017-03-06 11:31:06 +0800

    update user passwor error mta imap pop3

--- cmbin/user_password_error_check.sh
+++ cmbin/user_password_error_check.sh
@@ -3,10 +3,17 @@
 # E-Mail : danielfancy@gmail.com
 
 DATA=`date +"%Y_%m_%d"`
-CHECK_CMD=`grep "102(password error" /home/coremail/logs/udsvr.log | awk  '{print $7}' | sort | uniq -c | sort -rn | head -10`
+CHECK_MTA=`grep "Result:Failed" /home/coremail/logs/mtasvr.log | awk -F "," '{print $4"  \t "$6}' | sort`
+CHECK_POP=`grep "login fail" /home/coremail/logs/pop3svr.log | awk '{print $4"  "$5}' | sort`
+CHECK_IMAP=`grep "password error" /home/coremail/logs/imapsvr.log | awk '{print $4"  "$6}' | sort `
 TMP_USER=/tmp/user.txt
 
 [[ -f ${TMP_USER} ]] && \rm ${TMP_USER}  
-echo "${CHECK_CMD}" > ${TMP_USER}
+echo "MTA" >> ${TMP_USER}
+echo "${CHECK_MTA}" >> ${TMP_USER}
+echo "POP3" >> ${TMP_USER}
+echo "${CHECK_POP}" >> ${TMP_USER}
+echo "IMAP" >> ${TMP_USER}
+echo "${CHECK_IMAP}" >> ${TMP_USER}
 mail -s "Report Coremail mail system check password error user from `hostname` ${DATA}" danielfancy@gmail.com < ${TMP_USER} 
 
